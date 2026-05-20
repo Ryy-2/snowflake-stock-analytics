319d67b5e8cf8ab9531aceb9acf1cac6b217b884
Ry <Pinaryhak1411@gmail.com>
2026-05-20 14:01:57 +1000

    Update refresh_stock_data.py

--- scripts/refresh_stock_data.py
+++ scripts/refresh_stock_data.py
@@ -8,8 +8,7 @@ from datetime import datetime, timedelta
 conn = snowflake.connector.connect(
     account=os.environ["SNOWFLAKE_ACCOUNT"],
     user=os.environ["SNOWFLAKE_USER"],
-    token=os.environ["SNOWFLAKE_PASSWORD"],
-    authenticator='oauth',
+    password=os.environ["SNOWFLAKE_PASSWORD"],
     warehouse=os.environ["SNOWFLAKE_WAREHOUSE"],
     database=os.environ["SNOWFLAKE_DATABASE"],
     schema=os.environ["SNOWFLAKE_SCHEMA"]
